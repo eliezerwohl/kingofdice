f316ba62caf5c96131d71decc38384a3da63bae7
eliezerwohl <eliezerwohl@gmail.com>
2016-01-03 17:48:28 -0500

    fixed javascript

--- javascript/diceLogic.js
+++ javascript/diceLogic.js
@@ -1,37 +1,30 @@
 var rollOptions = ["hand", "heart", "lightning", "building", "skull", "heart"];
 var diceCount = 6;
+$(document).ready(function(){
+  $(".btn-primary").on("click", function(){
+  diceCount = $(this).attr('data-choice');
+  return diceCount;
+  });
 
-$(".btn-primary").on("click", function(){
-diceCount = $(this).attr('data-choice');
-console.log(diceCount);
-return diceCount
-});
-
-$("body").on("click", ".rollButton", function(){
- $( this ).toggleClass("stayRoll");
-});
-
-$("#rollIt").on("click", function(){
-  console.log(diceCount)
-  var n = $( ".stayRoll" ).length;
-
-var manyRolls = diceCount - n;
-// console.log(manyRolls)
-$( ".rollButton" ).not( ".stayRoll" ).remove();
-  for (i = 0; i < manyRolls; i++) { 
-   diceRolling();
-}
-
-function diceRolling(){
-var diceRoll = Math.floor(Math.random() * rollOptions.length)
-var rolled =  rollOptions[diceRoll];
-var rolledImg = rolled + ".png";
-var img = $ ("<img>").attr("src", rolledImg)
-
-var div =$("<button>").addClass("rollButton")
-.append(img);
-
-$(".target").append(div);
-}
+  $("body").on("click", ".rollButton", function(){
+  $( this ).toggleClass("stayRoll");
+  });
+  $("#rollIt").on("click", function(){
+    var n = $( ".stayRoll" ).length;
+    var manyRolls = diceCount - n;
+      $( ".rollButton" ).not( ".stayRoll" ).remove();
+        for (i = 0; i < manyRolls; i++) { 
+         diceRolling();
+  }
+  function diceRolling(){
+    var diceRoll = Math.floor(Math.random() * rollOptions.length)
+    var rolled =  rollOptions[diceRoll];
+    var rolledImg = rolled + ".png";
+    var img = $ ("<img>").attr("src", rolledImg)
+    var div =$("<button>").addClass("rollButton")
+    .append(img);
+    $(".target").append(div);
+    }
+  });
 });
 
