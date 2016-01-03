922362292c2ca543b025e7c1ce6b988423849c48
eliezerwohl <eliezerwohl@gmail.com>
2016-01-03 14:59:10 -0500

    removes only those choices you want to re-roll

--- javascript/diceLogic.js
+++ javascript/diceLogic.js
@@ -1,18 +1,32 @@
 var rollOptions = ["hand", "heart", "lightning", "building", "skull", "heart"];
 
+$( ".count" ).click(function() {
+      var n = $( ".stayRoll" ).length;
+      var j = $(".newRoll").length;
+   console.log(n)
+   console.log(j)
+  // $( this ).toggleClass( stayRoll, rollButton );
+});
+
+$("body").on("click", ".rollButton", function(){
+ $( this ).toggleClass("stayRoll");
+});
 
 $(".btn").on("click", function(){
-$(".target").empty();
-  for (i = 0; i < 6; i++) { 
+  var n = $( ".stayRoll" ).length;
+
+var manyRolls = 6 - n;
+// console.log(manyRolls)
+$( ".rollButton" ).not( ".stayRoll" ).remove();
+  for (i = 0; i < manyRolls; i++) { 
    diceRolling();
 }
 
 function diceRolling(){
 var diceRoll = Math.floor(Math.random() * rollOptions.length)
-console.log(rollOptions[diceRoll])
 var rolled =  rollOptions[diceRoll]
 
-var div =$("<button>").append(rolled)
+var div =$("<button>").addClass("rollButton").append(rolled);
 
 $(".target").append(div);
 }
