c0b110ba0850a1bf923f65167710b44453191dad
eliezerwohl <eliezerwohl@gmail.com>
2016-01-03 13:43:31 -0500

    button now generates 6 buttons

--- javascript/diceLogic.js
+++ javascript/diceLogic.js
@@ -1,1 +1,20 @@
-prompt ('herro world')
+var rollOptions = ["hand", "heart", "lightning", "building", "skull", "heart"];
+
+
+$(".btn").on("click", function(){
+$(".target").empty();
+  for (i = 0; i < 6; i++) { 
+   diceRolling();
+}
+
+function diceRolling(){
+var diceRoll = Math.floor(Math.random() * rollOptions.length)
+console.log(rollOptions[diceRoll])
+var rolled =  rollOptions[diceRoll]
+
+var div =$("<button>").append(rolled)
+
+$(".target").append(div);
+}
+});
+
